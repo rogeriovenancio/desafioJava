0df1528ba3c23e49be609a54cee2feb8b52fbffa
Rogerio <rogeriorvs@hotmail.com>
2022-08-23 19:39:07 -0300

    four commit

---
+++ desafioLogico.java
@@ -0,0 +1,30 @@
+package desafio;
+
+public class desafioLogico {
+	
+	public static void main(String[] args) {
+		// Trabalho na terça (V ou F)
+		// Trabalho na quinta (V ou F)
+		
+		boolean trabalho1 = false;
+		boolean trabalho2 = true;
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		
+		//Operador Unario!
+		boolean maisSaudavelFicarEmCasa = !comprouSorvete;
+		
+		System.out.println("comprou TV 50 \"? " + comprouTV50);
+		System.out.println("comprou TV 32 \"? " + comprouTV32);
+		System.out.println("comprou Sorvete? " + comprouSorvete);
+		System.out.println("Mais Saudavel Ficar Em Casa?  " + maisSaudavelFicarEmCasa);
+		
+		
+		
+		
+				
+	}
+
+}
